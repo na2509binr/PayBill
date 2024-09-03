853f55efa0ef879adce888dbda6989276b521836
Admin <fanqtv2412@gmail.com>
2024-09-03 22:13:36 +0700

    chore: add date time picker

---
+++ PayBill/Models/Spending.cs
@@ -0,0 +1,9 @@
+﻿namespace PayBill.Models;
+
+public class Spending
+{
+    public string Id_Spending { get; set; }
+    public string Spending_Name { get; set; }
+    public int Spending_Amount { get; set; }
+    public long Spending_Total { get; set; }
+}
